b565118a1d95caed2acdcfd1a3aec60c7e252c09
meryem862009-spec <meryem862009@gmail.com>
2025-10-15 13:17:08 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['*'] } }
+  reactStrictMode: true,
+  typescript: {
+    // Allow production builds even if there are TS errors
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Skip ESLint during builds on CI
+    ignoreDuringBuilds: true,
+  },
 };
 export default nextConfig;
